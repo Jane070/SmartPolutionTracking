d57112a0b1264d94f5a640e859f6153f62285943
Jane070 <zjane7887@gmail.com>
2023-04-11 00:14:44 +0100

    a little change on service 1

--- src/main/java/ds/client/GUIapplication.java
+++ src/main/java/ds/client/GUIapplication.java
@@ -4,18 +4,35 @@ import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceEvent;
+import javax.jmdns.ServiceInfo;
+import javax.jmdns.ServiceListener;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import ds.airqualityservice.*;
+import ds.airqualityservice.AirQualityServiceGrpc.AirQualityServiceBlockingStub;
+import ds.airqualityservice.AirQualityServiceGrpc.AirQualityServiceStub;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 
 public class GUIapplication {
 
+	private static AirQualityServiceBlockingStub blockingStub;
+	private static AirQualityServiceStub asyncStub;
+
+	private ServiceInfo airQualityServiceInfo;
+	
 	private JFrame frame;
 	private JTextField textNumber1;
 	private JTextField textNumber2;
@@ -37,11 +54,93 @@ public class GUIapplication {
 		});
 	}
 	
+	
+public GUIapplication() {
 		
+		String airquality_service_type = "_airqualityservice._tcp.local.";
+		discoverAirQualityService(airquality_service_type);
+		
+		String host = airQualityServiceInfo.getHostAddresses()[0];
+		int port = airQualityServiceInfo.getPort();
+		
+		ManagedChannel channel = ManagedChannelBuilder
+				.forAddress(host, port)
+				.usePlaintext()
+				.build();
+
+		//stubs -- generate from proto
+		blockingStub = AirQualityServiceGrpc.newBlockingStub(channel);
+
+		asyncStub = AirQualityServiceGrpc.newStub(channel);
+
+		
+		initialize();
+	}
+		
+
+private void discoverAirQualityService(String service_type) {
+	
+	
+	try {
+		// Create a JmDNS instance
+		JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+
+			
+		jmdns.addServiceListener(service_type, new ServiceListener() {
+			
+			@Override
+			public void serviceResolved(ServiceEvent event) {
+				System.out.println("Math Service resolved: " + event.getInfo());
+
+				airQualityServiceInfo = event.getInfo();
+
+				int port = airQualityServiceInfo.getPort();
+				
+				System.out.println("resolving " + service_type + " with properties ...");
+				System.out.println("\t port: " + port);
+				System.out.println("\t type:"+ event.getType());
+				System.out.println("\t name: " + event.getName());
+				System.out.println("\t description/properties: " + airQualityServiceInfo.getNiceTextString());
+				System.out.println("\t host: " + airQualityServiceInfo.getHostAddresses()[0]);
+			
+				
+			}
+			
+			@Override
+			public void serviceRemoved(ServiceEvent event) {
+				System.out.println("Math Service removed: " + event.getInfo());
+
+				
+			}
+			
+			@Override
+			public void serviceAdded(ServiceEvent event) {
+				System.out.println("Math Service added: " + event.getInfo());
+
+				
+			}
+		});
+		
+		// Wait a bit
+		Thread.sleep(2000);
+		
+//		jmdns.close();
+
+	} catch (UnknownHostException e) {
+		System.out.println(e.getMessage());
+	} catch (IOException e) {
+		System.out.println(e.getMessage());
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	
+}
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	public GUIapplication() {
+	private void initialize() {
 		// TODO Auto-generated constructor stub
 	
 		frame = new JFrame();
@@ -61,18 +160,26 @@ public class GUIapplication {
 		JButton btnCalculate = new JButton("Get latest reading about air quality");
 		btnCalculate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-//				
-//				int num1 = Integer.parseInt(textNumber1.getText());
-//				int num2 = Integer.parseInt(textNumber2.getText());
-//
-//				int index = comboOperation.getSelectedIndex();
-//				
-//				if (index == 0) { textResponse.append("Result of Addition" + String.valueOf(num1 + num2)); }
-//				if (index == 1) { textResponse.append("Result of Subtraction" + String.valueOf(num1 - num2)); }
-//				if (index == 2) { textResponse.append("Result of Multiplication" + String.valueOf(num1 * num2)); }
-//				if (index == 3) { textResponse.append("Result of Division" + String.valueOf(num1 / num2)); }
-//
+
+				GetLatestReadingRequest req = GetLatestReadingRequest.newBuilder().setLatestreading(null).build();
+
+				GetLatestReadingResponse response = blockingStub.getLatestReading(req);
+				
+				System.out.println("Message sent by the server " + response.getPm25());
+
 			}
 		});
+		
+        panel_service_1.add(btnCalculate);
+		
+		textResponse = new JTextArea(3, 20);
+		textResponse .setLineWrap(true);
+		textResponse.setWrapStyleWord(true);
+		
+		JScrollPane scrollPane = new JScrollPane(textResponse);
+		
+		//textResponse.setSize(new Dimension(15, 30));
+		panel_service_1.add(scrollPane);
+		
 }
 }
